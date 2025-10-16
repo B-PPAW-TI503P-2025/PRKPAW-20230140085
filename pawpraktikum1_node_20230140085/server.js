75f84b7b9b0e11074cf1fef568fbe9205a37dab2
febry40 <febryevayonisa@gmail.com>
2025-10-16 23:35:18 +0700

    first commit

---
+++ pawpraktikum1_node_20230140085/server.js
@@ -0,0 +1,43 @@
+// ===== Import Module =====
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+
+
+// ===== Middleware =====
+app.use(cors()); // izinkan akses dari React (frontend)
+app.use(express.json()); // parsing body JSON
+
+// Middleware custom untuk logging
+app.use((req, res, next) => {
+  console.log('${new Date().toISOString()} - ${req.method} ${req.url}');
+  next();
+});
+
+// ===== Routing dasar =====
+app.get('/', (req, res) => {
+  res.send('Home Page for API');
+});
+
+// ===== Routing CRUD Buku =====
+const bookRoutes = require('./routes/books');
+app.use('/api/books', bookRoutes);
+
+// ===== Middleware Error Handling =====
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
+});
+
+// ===== Jalankan Server =====
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
---
+++ tugas/Tugas2.md
@@ -0,0 +1,8 @@
+1.Tampilan End Point Read Book:
+    [alt text](image.png)
+3.Tampilan End Point Create Book:
+    [alt text](image-1.png)
+4.Tampilan End Point Update Book:
+   [alt text](image-2.png)
+5.Tampilan End Point Delete Book:
+   [alt text](image-3.png)
